938d6d5de1433f6124a7ee5a86f3363ca387a9f2
kyliehawes <uhaweky@indpkml-jcfdf8v.peroot.com>
2014-06-11 15:40:44 -0400

    added more tweens

--- interactives/Firewall/js/packet.js
+++ interactives/Firewall/js/packet.js
@@ -6,11 +6,16 @@ define(["Phaser"], function (Phaser) {
 		// Set the pivot point for this sprite to the center
 		this.anchor.setTo(0.5, 0.5);
 		
-		this.rotation = .52;
+		this.rotation = .6;
 		
 		// TODO: don't loop this (is there a way to repeat without looping?)
-		var tween = game.add.tween(this).to({ x: 250, y: 360, rotation: .5 }, 500, Phaser.Easing.Linear.None)
-			.to({ x: 450, y: 300, rotation: 0 }, 500, Phaser.Easing.Linear.None)
+		var tween = game.add.tween(this)
+			.to({ x: 150, y: 360, rotation: .6 }, 390, Phaser.Easing.Linear.None)
+			.to({ x: 200, y: 363, rotation: .52 }, 250, Phaser.Easing.Linear.None)
+			.to({ x: 260, y: 362, rotation: .41 }, 250, Phaser.Easing.Linear.None)
+			.to({ x: 320, y: 352, rotation: .3 }, 250, Phaser.Easing.Linear.None)
+			.to({ x: 385, y: 334, rotation: .15 }, 250, Phaser.Easing.Linear.None)
+			.to({ x: 450, y: 300, rotation: 0 }, 250, Phaser.Easing.Linear.None)
 			.start();
 		
 		// NOTE: hack to trigger onComplete after LAST tween (not the first)
