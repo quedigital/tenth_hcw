9edd0a81dee284fc8edcea00b94ec1bc1b1abb3d
kyliehawes <uhaweky@indpkml-jcfdf8v.peroot.com>
2014-08-05 18:06:47 -0400

    changed walking sprite speed

--- site/main.js
+++ site/main.js
@@ -17,8 +17,8 @@ requirejs.config({
 require(["jquery", "LayoutManager"], function ($, LayoutManager) {
 //	$.getJSON("export.json", null, onData);
 //	$.getJSON("https://res.cloudinary.com/hcw10/raw/upload/export.json", null, onData);
-	$.getJSON("file:///users/ubarrc6/Downloads/export.json", null, onData);
-//	$.getJSON("file:///users/uhaweky/Downloads/export.json", null, onData);
+//	$.getJSON("file:///users/ubarrc6/Downloads/export.json", null, onData);
+	$.getJSON("file:///users/uhaweky/Downloads/export.json", null, onData);
 
 	var layout = new LayoutManager("#container");
 	
