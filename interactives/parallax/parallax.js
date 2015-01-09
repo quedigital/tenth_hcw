66b67a666aecc71eca4be2bd04cb7595c8a3c1f3
Charlie Barrows <charles.barrows@pearson.com>
2015-01-09 16:56:33 -0500

    Parallax working

--- interactives/parallax/parallax.js
+++ interactives/parallax/parallax.js
@@ -13,7 +13,44 @@ requirejs.config({
 
 require(["jquery"], function () {
 
-    console.log("Hello");
-    console.log($("body"));
+    // left @ -240 = 0, 660 = 1
+
+    $("#eye-layer").mousemove(onEyeHover);
+
+    updateLeftEye();
+    updateRightEye();
+
+    function updateLeftEye () {
+        var x = $("#eyes").position().left;
+
+        var pct = (x + 240) / 900;
+
+        var w = $("#left-eye").width();
+
+        $("#left-eye .barista").css("left", -w * pct * 6.0 + 300);
+        $("#left-eye .table").css("left", -w * pct * 5.0 + 310);
+        $("#left-eye .counter").css("left", -w * pct * 4.0 + 180);
+        $("#left-eye .street").css("left", -w * pct * 1.8 - 315);
+    }
+
+    function updateRightEye () {
+        var x = $("#eyes").position().left;
+
+        var pct = (x + 240) / 900;
+
+        var w = $("#right-eye").width();
+
+        $("#right-eye .barista").css("left", -w * pct * 6.0 + 80);
+        $("#right-eye .table").css("left", -w * pct * 5.0 + 140);
+        $("#right-eye .counter").css("left", -w * pct * 4.0 - 10);
+        $("#right-eye .street").css("left", -w * pct * 1.8 - 380);
+    }
+
+    function onEyeHover (event) {
+        $("#eyes").offset({ left: event.pageX - 280 });
+
+        updateLeftEye();
+        updateRightEye();
+    }
 
 });
